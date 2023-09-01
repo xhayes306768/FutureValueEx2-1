5e74f2790d5e2827fec23d39b29b3745eac1019f
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-09-01 12:02:00 -0500

    Add project files.

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+    }
+}
---
+++ FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FutureValue.Models
+{
+    public class FutureValueModel
+    {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+        public decimal CalculateFutureValue()
+        {
+            int months = Years * 12;
+            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100; decimal futureValue = 0; for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+
+
+
+        }
+    }
+}
