bbfb8e82176c619b10c03ad3427ef6d57f99c4a4
Xavier Hayes <xavier.hayes@southeasttech.edu>
2023-09-03 02:12:23 -0500

    Added a  Razor layout named _Layout.cshtml to the Views/Shared folder and modifed it so it contains the code shown in figure 2-16. Also includes the link element for the site.css file.

--- FutureValue/Controllers/HomeController.cs
+++ FutureValue/Controllers/HomeController.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FutureValue.Controllers
 {
     public class HomeController : Controller
     {
+
+        [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Name = "Mary";
-            ViewBag.FV = 99999.99;
+            //ViewBag.Name = "Mary";
+            //  ViewBag.FV = 99999.99;
+            ViewBag.FV = 0;
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue(); return View(model);
         }
     }
 }
